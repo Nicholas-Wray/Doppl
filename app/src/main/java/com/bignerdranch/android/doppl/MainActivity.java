8392f25c94323d7229f01c496f1711c8bc8751e7
Nicholas-Wray <nicholas.wray@trincoll.edu>
2015-11-24 12:11:35 -0500

    Added readme for kairos logic and added the logic to onCreate, did break and does not compile but everything can be commented out

--- app/src/main/java/com/bignerdranch/android/doppl/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/doppl/MainActivity.java
@@ -28,6 +28,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+import com.kairos.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -35,12 +36,8 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
+
+
 public class MainActivity extends Activity {
 
     public static final int REQUEST_CAMERA = 1;
@@ -68,10 +65,163 @@ public class MainActivity extends Activity {
         image2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
-
                 selectImage();
             }
         });
+
+
+        // listener
+        KairosListener listener = new KairosListener() {
+
+            @Override
+            public void onSuccess(String response) {
+                Log.d("KAIROS DEMO", response);
+            }
+
+            @Override
+            public void onFail(String response) {
+                Log.d("KAIROS DEMO", response);
+            }
+        };
+
+
+        /* * * instantiate a new kairos instance * * */
+        Kairos myKairos = new Kairos();
+
+        /* * * set authentication * * */
+        String app_id = "10acb675";
+        String api_key = "3dd4660b913c705c2e99fb50ad1cf38b";
+        myKairos.setAuthentication(this, app_id, api_key);
+
+
+
+
+        try {
+
+
+            /* * * * * * * * * * * * * * * * * * * * */
+            /* * *  Kairos Method Call Examples * * */
+            /* * * * * * * * * * * * * * * * * * * */
+            /* * * * * * * * * * * * * * * * * * **/
+            /* * * * * * * * * * * * * * * * * * */
+            /* * * * * * * * * * * * * * * * * **/
+            /* * * * * * * * * * * * * * * * * */
+            /* * * * * * * * * * * * * * * * **/
+            /* * * * * * * * * * * * * * * * */
+
+
+            //  List galleries
+            myKairos.listGalleries(listener);
+
+
+            /* * * * * * * * DETECT EXAMPLES * * * * * * *
+
+
+            // Bare-essentials Example:
+            // This example uses only an image url, setting optional params to null
+            String image = "http://media.kairos.com/liz.jpg";
+            myKairos.detect(image, null, null, listener);
+
+
+
+            // Fine-grained Example:
+            // This example uses a bitmap image and also optional parameters
+            Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.liz);
+            String selector = "FULL";
+            String minHeadScale = "0.25";
+            myKairos.detect(image, selector, minHeadScale, listener);
+
+            */
+
+
+
+            /* * * * * * * * ENROLL EXAMPLES * * * * * * *
+
+            // Bare-essentials Example:
+            // This example uses only an image url, setting optional params to null
+            String image = "http://media.kairos.com/liz.jpg";
+            String subjectId = "Elizabeth";
+            String galleryId = "friends";
+            myKairos.enroll(image, subjectId, galleryId, null, null, null, listener);
+
+
+            // Fine-grained Example:
+            // This example uses a bitmap image and also optional parameters
+            Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.liz);
+            String subjectId = "Elizabeth";
+            String galleryId = "friends";
+            String selector = "FULL";
+            String multipleFaces = "false";
+            String minHeadScale = "0.25";
+            myKairos.enroll(image,
+                    subjectId,
+                    galleryId,
+                    selector,
+                    multipleFaces,
+                    minHeadScale,
+                    listener);
+
+                    */
+
+
+            /* * * * * * * RECOGNIZE EXAMPLES * * * * * * *
+
+            // Bare-essentials Example:
+            // This example uses only an image url, setting optional params to null
+            String image = "http://media.kairos.com/liz.jpg";
+            String galleryId = "friends";
+            myKairos.recognize(image, galleryId, null, null, null, null, listener);
+
+
+            // Fine-grained Example:
+            // This example uses a bitmap image and also optional parameters
+            Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.liz);
+            String galleryId = "friends";
+            String selector = "FULL";
+            String threshold = "0.75";
+            String minHeadScale = "0.25";
+            String maxNumResults = "25";
+            myKairos.recognize(image,
+                    galleryId,
+                    selector,
+                    threshold,
+                    minHeadScale,
+                    maxNumResults,
+                    listener);
+
+                    */
+
+
+            /* * * * GALLERY-MANAGEMENT EXAMPLES * * * *
+
+
+            //  List galleries
+            myKairos.listGalleries(listener);
+
+
+
+            //  List subjects in gallery
+            myKairos.listSubjectsForGallery("your_gallery_name", listener);
+
+
+
+            // Delete subject from gallery
+            myKairos.deleteSubject("your_subject_id", "your_gallery_name", listener);
+
+
+
+            // Delete an entire gallery
+            myKairos.deleteGallery("your_gallery_name", listener);
+
+            */
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
     }
 
 
