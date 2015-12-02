2b0d29eb8fa76e5dd13744a7ec291cb710195d0a
Nicholas-Wray <nicholas.wray@trincoll.edu>
2015-12-02 16:49:41 -0500

    fixed import statements for kairos api

--- app/src/main/java/com/bignerdranch/android/doppl/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/doppl/MainActivity.java
@@ -30,12 +30,15 @@ import android.widget.ImageView;
 import android.widget.Toast;
 import com.kairos.*;
 
+import org.json.JSONException;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 
 
 public class MainActivity extends Activity {
